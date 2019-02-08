08edb0dc499bc354aebe83d87e0538ca47ef6394
thanwaratt <thanwarat2639@gmail.com>
2019-02-08 22:12:05 +0700

    Create Output in ProjectA

--- src/Employee.java
+++ src/Employee.java
@@ -4,20 +4,23 @@ public class Employee {
 	private int priceMonth;
 	private int price;
 	private String name;
+	private double compen;
 	public Employee(int num, String n,int p,int pM) {
 		this.NumberEmployee = num;
 		this.priceMonth = pM;
 		this.price = p;
 		this.name = n;
 	}
-	public double compensation() {
-		double c = 0;
+	public double getCompensation() {
 		if( this.priceMonth >0 || this.priceMonth <= 50000) {
-			c = this.price + ((5/100)*this.price );
+			this.compen = this.price + ((5/100)*this.price );
 		}
-		else c = this.price + ((5/100)*this.price );
-		return c;
+		else this.compen = this.price + ((5/100)*this.price );
+		return this.compen;
 		
 	}
-
+	public String getName() {
+		return this.name;
+	}
+	
 }
--- src/output.java
+++ src/output.java
@@ -1,4 +1,13 @@
 
 public class output {
+	private Employee emp ;
+	public output(Employee e) {
+		this.emp = e;
+	}
+	
+	public void printEmp() {
+		System.out.println("Name : " + this.emp.getName() + " Compensation :" + this.emp.getCompensation());
+	}
+	
 
 }
